548474e2650a7305fbbe36226a0e983086436177
Vitória Maria de Souza Almeida Gomes <vitoria.gomes@engesoftware.com.br>
2026-01-12 08:53:06 -0300

    Refatoração de meta-model para similaridade com o Productivity-PHP

--- src/models/Meta.ts
+++ src/models/Meta.ts
@@ -2,28 +2,42 @@ import {
     Entity,
     PrimaryGeneratedColumn,
     Column,
-    ManyToOne, OneToMany
+    ManyToOne, OneToMany, CreateDateColumn, UpdateDateColumn
 } from 'typeorm'
 import {User} from "./User";
 import {Tarefa} from "./Tarefa"
-import {IsDateString, MinDate} from "class-validator";
+import {IsDateString, IsInt, IsOptional, MinDate} from "class-validator";
 import {Type} from "class-transformer";
 
-@Entity()
+@Entity('metas')
 export class Meta{
     @PrimaryGeneratedColumn()
     id!:number
 
     @Column()
+    titulo!:string
+
+    @Column()
     descricao!:string
 
-    @Column({type:'date'})
-    @IsDateString({}, { message: 'data_inicio deve ser uma data ISO válida (yyyy-MM-dd)' })
-    data_inicio!:string
+    @Column({type:'tinyint', default:1, unsigned: true})
+    @IsInt()
+    prioridade!:number
 
-    @Column({type:'date'})
-    @IsDateString({}, { message: 'data_fim deve ser uma data ISO válida (yyyy-MM-dd)' })
-    data_fim!:string
+    @Column({type: 'date'})
+    @IsDateString({}, { message: 'data_expiracao deve ser uma data ISO válida (yyyy-MM-dd)' })
+    data_expiracao!:string
+
+    @Column({type: 'datetime', nullable: true})
+    @IsOptional()
+    @IsDateString()
+    concluida_em?:string|null;
+
+    @CreateDateColumn()
+    created_at!:Date;
+
+    @UpdateDateColumn()
+    updated_at!:Date;
 
     @ManyToOne(() => User,(user) => user.metas,{onDelete:'CASCADE'})
     user!:User
